6fc8bd5f4cf447469fc2a513056deb0a476f6319
vijay narayanan <vijaynarayanan027@gmail.com>
2026-04-19 20:07:27 +0530

    Updated code with my changes

--- BlackScholes-main/streamlit_app.py
+++ BlackScholes-main/streamlit_app.py
@@ -123,10 +123,7 @@ class BlackScholes:
 # Sidebar for User Inputs
 with st.sidebar:
     st.title("📊 Black-Scholes Model")
-    st.write("`Created by:`")
-    linkedin_url = "https://www.linkedin.com/in/mprudhvi/"
-    st.markdown(f'<a href="{linkedin_url}" target="_blank" style="text-decoration: none; color: inherit;"><img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" width="25" height="25" style="vertical-align: middle; margin-right: 10px;">`Prudhvi Reddy, Muppala`</a>', unsafe_allow_html=True)
-
+    
     current_price = st.number_input("Current Asset Price", value=100.0)
     strike = st.number_input("Strike Price", value=100.0)
     time_to_maturity = st.number_input("Time to Maturity (Years)", value=1.0)
